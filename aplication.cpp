24d8c138b6b29f01e738860116723197b99a2bfa
Illya Stolpakov <settnozz@yandex.ua>
2016-05-12 21:44:53 +0300

    Update aplication.cpp

--- aplication.cpp
+++ aplication.cpp
@@ -18,45 +18,60 @@ double val_cur_val(double, char, double, double, int);
 double** check_function(double, double, double);
 void write_to_file(double**, int);
 
-int main() {
+int main() 
+{
 	chose_enter();
 	system("pause");
 	return 0;
 }
 
-void chose_enter() {
+void chose_enter() 
+{
 	char number;
 	cout << "If you want write value at kayboard, enter 1." << endl << "If you want taken from the file, enter 2." << endl;
 	cin >> number;
 	switch (number){
-	case '1': keyboard(); break;
-	case '2': read_file(x_beg, x_end, x_step, precision); break;
+	case '1': keyboard(); 
+		break;
+	case '2': read_file(x_beg, x_end, x_step, precision); 
+		break;
 		default: cout << "Your input data is incorrect!"; 
 		break;
 	}
 }
 
-void keyboard() {
-	cout << "Enter X(begin): "; cin >> x_beg;
-	cout << "Enter X(end): "; cin >> x_end;
-	cout << "Enter step of x: "; cin >> x_step;
-	cout << "Enter precision: "; cin >> precision;
+void keyboard() 
+{
+	cout << "Enter X(begin): "; 
+	cin >> x_beg;
+	cout << "Enter X(end): "; 
+	cin >> x_end;
+	cout << "Enter step of x: "; 
+	cin >> x_step;
+	cout << "Enter precision: "; 
+	cin >> precision;
 	calculation(x_beg, x_end, x_step, precision);
 	
 }
 
-void read_file(double& x_beg, double& x_end, double& x_step, double& precision) {
+void read_file(double& x_beg, double& x_end, double& x_step, double& precision) 
+{
 	ifstream input("input_file.txt");
 
-	input >> x_beg; cout << "X(begin): " << x_beg << endl;
-	input >> x_end; cout << "X(end): " << x_end << endl;
-	input >> x_step; cout << "Step of x: " << x_step << endl;
-	input >> precision; cout << "Precision: " << precision << endl;
+	input >> x_beg; 
+	cout << "X(begin): " << x_beg << endl;
+	input >> x_end; 
+	cout << "X(end): " << x_end << endl;
+	input >> x_step; 
+	cout << "Step of x: " << x_step << endl;
+	input >> precision; 
+	cout << "Precision: " << precision << endl;
 	input.close();
 	calculation(x_beg, x_end, x_step, precision);
 }
 
-void calculation(double, double, double, double) {
+void calculation(double, double, double, double) 
+{
 	double** result = cos_pow_3(x_beg, x_end, x_step, precision);
 	write_to_file(result, (int)((x_end - x_beg) / x_step) + 1);
 	double** check_res = check_function(x_beg, x_end, x_step);
@@ -64,7 +79,8 @@ void calculation(double, double, double, double) {
 	output(check_res, (int)((x_end - x_beg) / x_step) + 1);
 }
 
-double** cos_pow_3 (double x_beg, double x_end, double x_step, double precision) {
+double** cos_pow_3 (double x_beg, double x_end, double x_step, double precision) 
+{
 	double **result = new double*[(int)((x_end - x_beg) / x_step) + 1];
 	for (int i = 0; i < (int)((x_end - x_beg) / x_step) + 1; ++i)
 		result[i] = new double[2];
@@ -80,12 +96,14 @@ double** cos_pow_3 (double x_beg, double x_end, double x_step, double precision)
 	return result;
 }
 
-double factorial(int n) {
+double factorial(int n) 
+{
 	if (n == 0) return 1;
 	return n * (factorial(n - 1));
 }
 
-void output(double** arr, int lines) {
+void output(double** arr, int lines) 
+{
 	cout << "Massive of elements is: \n";
 	for (int i = 0; i < lines; i++)
 	{
@@ -97,7 +115,8 @@ void output(double** arr, int lines) {
 	}
 }
 
-double val_cur_val(double x, char sign, double precision, double cur_val, int i) {
+double val_cur_val(double x, char sign, double precision, double cur_val, int i) 
+{
 	if (fabs(cur_val) <= precision) 
 		return 0;
 	long double y_val = (pow(x, 2 * i)) / factorial(2 * i);
@@ -106,7 +125,8 @@ double val_cur_val(double x, char sign, double precision, double cur_val, int i)
 
 }
 
-double** check_function(double b, double e, double s) {
+double** check_function(double b, double e, double s) 
+{
 	double **result = new double*[(int)((e - b) / s) + 1];
 	for (int i = 0; i < (int)((e - b) / s) + 1; ++i)
 		result[i] = new double[2];
@@ -117,7 +137,8 @@ double** check_function(double b, double e, double s) {
 	return result;
 }
 
-void write_to_file(double** mas, int lines) {
+void write_to_file(double** mas, int lines) 
+{
 	ofstream fout;
 	fout.open("result.txt");
 	fout << "Massive of elements is: \n";
